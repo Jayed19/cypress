7f2ba29fef247a92b854965faab787576484438c
Jayed <jayed19@gmail.com>
2022-12-10 02:27:35 +0600

    keyboard action and another url browse option added

--- SearchFromGoogle_FirstProject_test_cy_js/test.cy.js
+++ SearchFromGoogle_FirstProject_test_cy_js/test.cy.js
@@ -1,8 +1,23 @@
-it('Google Search', {defaultCommandTimeout: 10000} ,() => {
+it('Google Search', {defaultCommandTimeout: 20000} ,() => {
     cy.visit('https://www.google.com/')
     cy.get('.gLFyf').type('Jayed Ibrahim')
     cy.contains('Google Search').click()
     cy.contains('Jayed Ibrahim - QA Manager').click()
+
+
+
+    cy.origin('https://www.linkedin.com', () => {
+  //cy.visit('/')
+  cy.get('input[name="email-or-phone"]').type("jayed{enter}{enter}")
+  cy.get('.join-form > .alert > .wrapper > .alert-content').should('have.text','Please enter a valid email address or mobile number.'); //# means id and xpath is showing with > sign
+
+})
+    
+    
+    
+  
+    
+
 
 
   })
